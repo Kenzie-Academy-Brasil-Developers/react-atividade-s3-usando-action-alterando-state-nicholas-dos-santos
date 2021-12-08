0ffcf0d7c3359833a84c4d070d77045080ba99b6
nicholas <nicholassantos2017@gmail.com>
2021-12-08 17:42:09 -0300

    fix: new style

--- src/components/UserCard/index.jsx
+++ src/components/UserCard/index.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeName } from "../../store/modules/user/actions";
+import { Btn } from "../style";
 
 const UserCard = () => {
   const [inputName, setInputName] = useState("");
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
 
-  const handleClick = () => dispatch(changeName(inputName));
+  const handleClick = () => inputName !== "" && dispatch(changeName(inputName));
 
   return (
     <div>
       <h2>User Name: {user.name}</h2>
       <input type="text" onChange={(e) => setInputName(e.target.value)} />
-      <button onClick={handleClick}>Change</button>
+      <Btn onClick={handleClick}>Change</Btn>
     </div>
   );
 };
---
+++ src/components/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Btn = styled.button`
+  width: 100px;
+  height: 35px;
+  background-color: azure;
+  border-radius: 10px;
+  margin: 10px;
+`;
